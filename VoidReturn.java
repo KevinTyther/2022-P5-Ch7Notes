6b6c2b8dfd194b5ca2fa6e524876eca69affd477
Kevin Tyther <kevin.tyther@vtsd.com>
2022-11-29 13:11:39 -0500

    sdaf

--- VoidReturn.java
+++ VoidReturn.java
@@ -1,8 +1,78 @@
-public class VoidReturn {
+import java.util.Scanner;
 
-   public static void main(String args[]){
-       
+public class VoidReturn
+{
+   public static void main(String args[])
+   {
+      int k = 0;
+      while (k < 2)
+      {
+         test();
+         for (int p = 0; p < 100; p++)
+         {
+            System.out.println("....");
+         }
+      }
 
+   }
+   public static void addNums()
+   {
+      System.out.println("We are going to add two numbers");
+      System.out.println("Please enter them below");
+
+      Scanner scan = new Scanner(System.in);
+      int num1 = scan.nextInt();
+      int num2 = scan.nextInt();
+
+      System.out.println("Your total is: " + (num1 + num2));
+   }
+
+   public static void subNums()
+   {
+      System.out.println("We are going to minus two numbers");
+      System.out.println("Please enter them below");
+
+      Scanner scan = new Scanner(System.in);
+      int num1 = scan.nextInt();
+      int num2 = scan.nextInt();
+
+      System.out.println("Your total is: " + (num1 - num2));
+   }
+
+   public static void mulNums()
+   {
+      System.out.println("We are going to multiply two numbers");
+      System.out.println("Please enter them below");
+
+      Scanner scan = new Scanner(System.in);
+      int num1 = scan.nextInt();
+      int num2 = scan.nextInt();
+
+      System.out.println("Your total is: " + (num1 * num2));
+   }
+
+
+   public static void test()
+   {
+      System.out.println("Test");
+      Scanner scan = new Scanner(System.in);
+      int x = scan.nextInt();
+      if (x == 1)
+      {
+         addNums();
+      }
+      if (x == 2)
+      {
+         subNums();
+      }
+      if (x == 3)
+      {
+         mulNums();
+      }
+      else
+      {
+         test();
+      }
    }
 
 }
